33895b2d1c298e976a64b9689a2a013236a59be5
Uwe Rosner <oscottmusic@posteo.de>
2018-11-15 10:15:06 +0100

    Changed OpenFilesWindow testfile and added example pictures of some DiffTools

--- testfiles/OpenFilesWindow-changed.cpp
+++ testfiles/OpenFilesWindow-changed.cpp
@@ -1,24 +1,9 @@
-#include <string.h>
-#include <stdio.h>
-
-#include <clib/alib_protos.h>
-#include <clib/dos_protos.h>
-#include <clib/gadtools_protos.h>
-#include <clib/graphics_protos.h>
-#include <clib/intuition_protos.h>
-#include <intuition/intuition.h>
-#include <intuition/gadgetclass.h>
-#include <intuition/imageclass.h>
-#include <intuition/icclass.h>
-#include <libraries/gadtools.h>
-
 #include "AslFileRequest.h"
 #include "OpenFilesWindow.h"
 
 OpenFilesWindow::OpenFilesWindow(AppScreen& p_AppScreen,
     struct MsgPort* p_pMsgPort, AmigaDiffFacade& p_DiffFacade)
   : WindowBase(p_AppScreen, p_pMsgPort),
-    m_bFileRequestOpen(false),
     m_DiffFacade(p_DiffFacade),
     m_pGadgetList(NULL),
     m_pLeftFileStringGadget(NULL),
@@ -28,13 +13,8 @@ OpenFilesWindow::OpenFilesWindow(AppScreen& p_AppScreen,
     m_pDiffButton(NULL),
     m_pCancelButton(NULL)
 {
-
-}
-
-OpenFilesWindow::~OpenFilesWindow()
-{
-  Close();
-
+  // We moved this into the constructor
+  // From the destructor. That'll be hilarious.
   if(m_pGadgetList != NULL)
   {
     FreeGadgets(m_pGadgetList);
@@ -48,13 +28,20 @@ OpenFilesWindow::~OpenFilesWindow()
   }
 }
 
+OpenFilesWindow::~OpenFilesWindow()
+{
+  Close();
+}
 
-
+OpenFilesWindow::NewMethod()
+{
+  // This method is completely new and shiny.
+}
 
 
 void OpenFilesWindow::Refresh()
 {
-// TODO
+
 //  BeginRefresh(m_pWindow);
 //  EndRefresh(m_pWindow, TRUE);
 }
@@ -166,14 +153,6 @@ void OpenFilesWindow::HandleIdcmp(ULONG p_Class, UWORD p_Code, APTR p_IAddress)
         // Text in right file string gadget was changed
         readStringGadgetsText();
       }
-      break;
-    }
-
-    case IDCMP_REFRESHWINDOW:
-    {
-      // This handling is REQUIRED with GadTools
-      GT_BeginRefresh(IntuiWindow());
-      GT_EndRefresh(IntuiWindow(), TRUE);
       break;
     }
 
@@ -497,4 +476,178 @@ void OpenFilesWindow::readStringGadgetsText()
   // If not not: disable it.
   setDiffButtonState();
 
+}
+
+
+#include <clib/gadtools_protos.h>
+#include <clib/intuition_protos.h>
+
+#include "AppMenu.h"
+
+
+AppMenu::AppMenu(AppScreen& p_Screen)
+  : m_Screen(p_Screen),
+    m_pMenu(NULL)
+{
+
+}
+
+AppMenu::~AppMenu()
+{
+  Dispose();
+}
+
+bool AppMenu::Create(struct NewMenu* p_pMenuDefinition)
+{
+  if(m_Screen.GadtoolsVisualInfo() == NULL)
+  {
+    // Without VisualInfo the menu can't be created
+    return false;
+  }
+
+  // Menu building step 1: Create the menu
+  m_pMenu = CreateMenus(p_pMenuDefinition, TAG_END);
+  if(m_pMenu == NULL)
+  {
+    // Menu creation failed
+    return false;
+  }
+
+  // Menu building step 2: Outlaying the menu
+  if(LayoutMenus(m_pMenu, m_Screen.GadtoolsVisualInfo(),
+                 GTMN_NewLookMenus, TRUE, // Ignored before v39
+                 TAG_END) == FALSE)
+  {
+    // Outlaying the menu failed
+    Dispose();
+    return false;
+  }
+
+  return true;
+}
+
+void AppMenu::Dispose()
+{
+  if(m_pMenu != NULL)
+  {
+    FreeMenus(m_pMenu);
+    m_pMenu = NULL;
+  }
+}
+
+bool AppMenu::AttachToWindow(struct Window* p_pWindow)
+{
+  if(SetMenuStrip(p_pWindow, m_pMenu) == FALSE)
+  {
+    // Binding the menu to given window failed
+    return false;
+  }
+
+  return true;
+}
+
+bool AppMenu::UpdateInWindow(struct Window* p_pWindow)
+{
+  if(ResetMenuStrip(p_pWindow, m_pMenu) == FALSE)
+  {
+    // Binding the menu to given window failed
+    return false;
+  }
+
+  return true;
+}
+
+void AppMenu::DetachFromWindow(struct Window* p_pWindow)
+{
+  ClearMenuStrip(p_pWindow);
+}
+
+struct Menu* AppMenu::IntuiMenu()
+{
+  return m_pMenu;
+}
+
+
+void AppMenu::DisableMenuItem(struct Window* p_pWindow,
+  APTR p_pUserDataMenuItemToDisable)
+{
+  if(p_pUserDataMenuItemToDisable == NULL)
+  {
+    return;
+  }
+
+  WORD menuNumber = 0;
+  struct MenuItem* pFoundItem = findItemByUserData(p_pUserDataMenuItemToDisable, menuNumber);
+  if(pFoundItem == NULL)
+  {
+    return;
+  }
+
+  OffMenu(p_pWindow, menuNumber);
+}
+
+void AppMenu::EnableMenuItem(struct Window* p_pWindow,
+  APTR p_pUserDataMenuItemToEnable)
+{
+  if(p_pUserDataMenuItemToEnable == NULL)
+  {
+    return;
+  }
+
+  WORD menuNumber = 0;
+  struct MenuItem* pFoundItem = findItemByUserData(p_pUserDataMenuItemToEnable, menuNumber);
+  if(pFoundItem == NULL)
+  {
+    return;
+  }
+
+  OnMenu(p_pWindow, menuNumber);
+}
+
+struct MenuItem* AppMenu::findItemByUserData(APTR p_pUserDataToFind, WORD& p_FoundMenuNumber)
+{
+  if(m_pMenu == NULL)
+  {
+    return NULL;
+  }
+
+  p_FoundMenuNumber = 0;
+
+  struct Menu* pMenu = m_pMenu;
+  struct MenuItem* pItem = pMenu->FirstItem;
+  if(pItem == NULL)
+  {
+    return NULL;
+  }
+
+  int iMenu = 0;
+  int iItem = 0;
+
+  do
+  {
+    do
+    {
+      APTR pUserData = GTMENUITEM_USERDATA(pItem);
+      if(pUserData == p_pUserDataToFind)
+      {
+        p_FoundMenuNumber = FULLMENUNUM(iMenu, iItem, 0);
+        return pItem;
+      }
+
+      pItem = pItem->NextItem;
+      iItem++;
+    }
+    while(pItem != NULL);
+
+    pMenu = pMenu->NextMenu;
+    if(pMenu != NULL)
+    {
+      pItem = pMenu->FirstItem;
+      iItem = 0;
+      iMenu++;
+    }
+  }
+  while(pItem != NULL);
+
+  return NULL;
 }
--- testfiles/OpenFilesWindow-original.cpp
+++ testfiles/OpenFilesWindow-original.cpp
@@ -1,17 +1,3 @@
-#include <string.h>
-#include <stdio.h>
-
-#include <clib/alib_protos.h>
-#include <clib/dos_protos.h>
-#include <clib/gadtools_protos.h>
-#include <clib/graphics_protos.h>
-#include <clib/intuition_protos.h>
-#include <intuition/intuition.h>
-#include <intuition/gadgetclass.h>
-#include <intuition/imageclass.h>
-#include <intuition/icclass.h>
-#include <libraries/gadtools.h>
-
 #include "AslFileRequest.h"
 #include "OpenFilesWindow.h"
 
@@ -166,6 +152,14 @@ void OpenFilesWindow::HandleIdcmp(ULONG p_Class, UWORD p_Code, APTR p_IAddress)
         // Text in right file string gadget was changed
         readStringGadgetsText();
       }
+      break;
+    }
+
+    case IDCMP_REFRESHWINDOW:
+    {
+      // This handling is REQUIRED with GadTools
+      GT_BeginRefresh(IntuiWindow());
+      GT_EndRefresh(IntuiWindow(), TRUE);
       break;
     }
 
@@ -489,178 +483,4 @@ void OpenFilesWindow::readStringGadgetsText()
   // If not not: disable it.
   setDiffButtonState();
 
-}
-
-
-#include <clib/gadtools_protos.h>
-#include <clib/intuition_protos.h>
-
-#include "AppMenu.h"
-
-
-AppMenu::AppMenu(AppScreen& p_Screen)
-  : m_Screen(p_Screen),
-    m_pMenu(NULL)
-{
-
-}
-
-AppMenu::~AppMenu()
-{
-  Dispose();
-}
-
-bool AppMenu::Create(struct NewMenu* p_pMenuDefinition)
-{
-  if(m_Screen.GadtoolsVisualInfo() == NULL)
-  {
-    // Without VisualInfo the menu can't be created
-    return false;
-  }
-
-  // Menu building step 1: Create the menu
-  m_pMenu = CreateMenus(p_pMenuDefinition, TAG_END);
-  if(m_pMenu == NULL)
-  {
-    // Menu creation failed
-    return false;
-  }
-
-  // Menu building step 2: Outlaying the menu
-  if(LayoutMenus(m_pMenu, m_Screen.GadtoolsVisualInfo(),
-                 GTMN_NewLookMenus, TRUE, // Ignored before v39
-                 TAG_END) == FALSE)
-  {
-    // Outlaying the menu failed
-    Dispose();
-    return false;
-  }
-
-  return true;
-}
-
-void AppMenu::Dispose()
-{
-  if(m_pMenu != NULL)
-  {
-    FreeMenus(m_pMenu);
-    m_pMenu = NULL;
-  }
-}
-
-bool AppMenu::AttachToWindow(struct Window* p_pWindow)
-{
-  if(SetMenuStrip(p_pWindow, m_pMenu) == FALSE)
-  {
-    // Binding the menu to given window failed
-    return false;
-  }
-
-  return true;
-}
-
-bool AppMenu::UpdateInWindow(struct Window* p_pWindow)
-{
-  if(ResetMenuStrip(p_pWindow, m_pMenu) == FALSE)
-  {
-    // Binding the menu to given window failed
-    return false;
-  }
-
-  return true;
-}
-
-void AppMenu::DetachFromWindow(struct Window* p_pWindow)
-{
-  ClearMenuStrip(p_pWindow);
-}
-
-struct Menu* AppMenu::IntuiMenu()
-{
-  return m_pMenu;
-}
-
-
-void AppMenu::DisableMenuItem(struct Window* p_pWindow,
-  APTR p_pUserDataMenuItemToDisable)
-{
-  if(p_pUserDataMenuItemToDisable == NULL)
-  {
-    return;
-  }
-
-  WORD menuNumber = 0;
-  struct MenuItem* pFoundItem = findItemByUserData(p_pUserDataMenuItemToDisable, menuNumber);
-  if(pFoundItem == NULL)
-  {
-    return;
-  }
-
-  OffMenu(p_pWindow, menuNumber);
-}
-
-void AppMenu::EnableMenuItem(struct Window* p_pWindow,
-  APTR p_pUserDataMenuItemToEnable)
-{
-  if(p_pUserDataMenuItemToEnable == NULL)
-  {
-    return;
-  }
-
-  WORD menuNumber = 0;
-  struct MenuItem* pFoundItem = findItemByUserData(p_pUserDataMenuItemToEnable, menuNumber);
-  if(pFoundItem == NULL)
-  {
-    return;
-  }
-
-  OnMenu(p_pWindow, menuNumber);
-}
-
-struct MenuItem* AppMenu::findItemByUserData(APTR p_pUserDataToFind, WORD& p_FoundMenuNumber)
-{
-  if(m_pMenu == NULL)
-  {
-    return NULL;
-  }
-
-  p_FoundMenuNumber = 0;
-
-  struct Menu* pMenu = m_pMenu;
-  struct MenuItem* pItem = pMenu->FirstItem;
-  if(pItem == NULL)
-  {
-    return NULL;
-  }
-
-  int iMenu = 0;
-  int iItem = 0;
-
-  do
-  {
-    do
-    {
-      APTR pUserData = GTMENUITEM_USERDATA(pItem);
-      if(pUserData == p_pUserDataToFind)
-      {
-        p_FoundMenuNumber = FULLMENUNUM(iMenu, iItem, 0);
-        return pItem;
-      }
-
-      pItem = pItem->NextItem;
-      iItem++;
-    }
-    while(pItem != NULL);
-
-    pMenu = pMenu->NextMenu;
-    if(pMenu != NULL)
-    {
-      pItem = pMenu->FirstItem;
-      iItem = 0;
-      iMenu++;
-    }
-  }
-  while(pItem != NULL);
-
-  return NULL;
 }
