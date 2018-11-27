fe741bbb22392a8a89333c461c7554429458448b
Uwe Rosner <oscottmusic@posteo.de>
2018-11-27 18:32:38 +0100

    Switehced main screen font to fixed size

--- gui/AppScreen.cpp
+++ gui/AppScreen.cpp
@@ -104,7 +104,7 @@ bool AppScreen::Open(ScreenModeEasy p_ScreenModeEasy)
       SA_AutoScroll, TRUE,
       SA_Pens, (ULONG)pWbDrawInfo->dri_Pens,
       // SA_Font, (ULONG) &m_TextAttr,
-      SA_SysFont, 1,  // Check if this fits
+      SA_SysFont, 0,  // Check if this fits
       SA_DisplayID, wbScreenModeId,
       SA_Title, m_Title.C_str(),
       TAG_DONE);
