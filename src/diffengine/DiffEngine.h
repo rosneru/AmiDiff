d89e8e167fb3d6f04189af36e04de7f71aa505a7
Uwe Rosner <oscottmusic@posteo.de>
2019-07-18 10:26:59 +0200

    Implemented shortestEdit method for myers diff

--- src/diffengine/DiffEngine.h
+++ src/diffengine/DiffEngine.h
@@ -7,27 +7,23 @@
 #include "ProgressReporter.h"
 
 /**
- * Class for generating a diff of the contents of two files.
+ * Class for generating and representing the difference of two files.
  *
- * This is a port of Stephane Rodriguez source code on CodeProject:
- * https://www.codeproject.com/Articles/3666/Diff-tool
- *
- * In 2005 he released an updated version:
- * http://www.arstdesign.com/articles/difftool.html
+ * This is an implementation of the myers diff algorithm.
  *
  * @author Uwe Rosner
- * @date 28/09/2018
+ * @date 18/07/2019
  */
 class DiffEngine
 {
 
 public:
-  DiffEngine(bool& p_bCancelRequested);
+  DiffEngine(bool& bCancelRequested);
 
-  bool Diff(DiffFilePartition& p_File1Src,
-            DiffFilePartition& p_File2Src,
-            DiffFilePartition& p_File1Diff,
-            DiffFilePartition& p_File2Diff);
+  bool Diff(DiffFilePartition& srcA,
+            DiffFilePartition& srcB,
+            DiffFilePartition& targetA,
+            DiffFilePartition& targetB);
 
   /**
    * Setting the progress reporter
@@ -37,6 +33,8 @@ public:
 private:
   bool& m_bCancelRequested;
   ProgressReporter* m_pProgressReporter;  ///> for progress reporting
+
+  int shortestEdit(DiffFilePartition& a, DiffFilePartition& b);
 
 };
 
