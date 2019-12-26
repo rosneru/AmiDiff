d0d50faf99eb20282133c0dad11f9caa4cf387e5
Uwe Rosner <oscottmusic@posteo.de>
2019-12-26 18:42:26 +0100

    Re-created progress bar using IText

--- src/gui/ProgressWindow.cpp
+++ src/gui/ProgressWindow.cpp
@@ -24,10 +24,10 @@ ProgressWindow::ProgressWindow(AScreen& appScreen,
     m_pGadtoolsContext(NULL),
     m_pGadTxtDescription(NULL),
     m_pGadBtnCancel(NULL),
-    m_ProgressBarLeft(NULL),
-    m_ProgressBarTop(NULL),
-    m_ProgressBarWidth(NULL),
-    m_ProgressBarHeight(NULL),
+    m_ProgressBarLeft(0),
+    m_ProgressBarTop(0),
+    m_ProgressBarWidth(0),
+    m_ProgressBarHeight(0),
     m_ProgressValueIText()
 {
 
@@ -79,15 +79,6 @@ bool ProgressWindow::Open(const APTR pMenuItemDisableAtOpen,
   GT_SetGadgetAttrs(m_pGadBtnCancel, IntuiWindow(), NULL,
     GA_Disabled, FALSE,
     TAG_END);
-
-  // Initialize the intui text structure for progress value display
-  m_ProgressValueIText.FrontPen  = m_AScreen.Pens().HighlightedText();
-  m_ProgressValueIText.BackPen   = m_AScreen.Pens().Background();
-  m_ProgressValueIText.DrawMode  = JAM1;
-  m_ProgressValueIText.LeftEdge  = (m_ProgressBarWidth / 2) + m_ProgressBarLeft;
-  m_ProgressValueIText.TopEdge   = m_ProgressBarTop + 1;
-  m_ProgressValueIText.ITextFont = NULL;
-  m_ProgressValueIText.NextText  = NULL;
 
   return true;
 }
@@ -198,8 +189,10 @@ void ProgressWindow::HandleProgress(struct ProgressMessage* pProgrMsg)
 
   m_ProgressValueIText.IText = (UBYTE*) m_ProgressValue.C_str();
 
+  // Calculate dX to print the text centered
   LONG textLength = IntuiTextLength(&m_ProgressValueIText);
-  ULONG dX = (m_ProgressBarWidth - textLength) / 2;
+  ULONG dX = textLength / 2;
+
 
   PrintIText(m_pWindow->RPort, &m_ProgressValueIText, -dX, 0);
 }
@@ -207,20 +200,18 @@ void ProgressWindow::HandleProgress(struct ProgressMessage* pProgrMsg)
 
 void ProgressWindow::initialize()
 {
-  //
-  // Calculate some basic values
-  //
   m_WinWidth = (WORD)m_AScreen.IntuiScreen()->Width / 2;
   m_FontHeight = m_AScreen.FontHeight();
   WORD barHeight = m_AScreen.IntuiScreen()->WBorTop + m_FontHeight + 2;
 
   WORD hSpace = 10;
-  WORD vSpace = 10;
+  WORD vSpace = 6;
+
   WORD top = barHeight + vSpace;
   WORD left = hSpace;
   WORD right = m_WinWidth - hSpace;
   WORD buttonWidth = 60;
-  WORD buttonHeight = m_FontHeight + 6;
+  WORD buttonHeight = m_FontHeight + vSpace;
   WORD stringGadgetWidth = right - left - hSpace / 2 - buttonWidth;
 
   //
@@ -248,20 +239,16 @@ void ProgressWindow::initialize()
   newGadget.ng_TopEdge    = top;
   newGadget.ng_Width      = stringGadgetWidth;
   newGadget.ng_Height     = buttonHeight;
-//  newGadget.ng_GadgetText = NULL;
   newGadget.ng_GadgetID   = GID_TxtDescription;
   newGadget.ng_Flags      = 0;
 
   m_pGadTxtDescription = CreateGadget(TEXT_KIND,
-                                     pFakeGad, &newGadget,
-                                     TAG_END);
+                                      pFakeGad, &newGadget,
+                                      TAG_END);
 
   // Preparing the newGadget struct for the progress value gadget
   newGadget.ng_LeftEdge   = left;
   newGadget.ng_TopEdge    += buttonHeight + vSpace;
-  newGadget.ng_Width      = stringGadgetWidth;
-  newGadget.ng_Height     = buttonHeight;
-  newGadget.ng_Flags      = 0;
 
   // But actually the "gadget" is just a BevelBox which is drawn after
   // window opening. So just remembering the progress gadget dimensions
@@ -287,13 +274,12 @@ void ProgressWindow::initialize()
   m_WinHeight = newGadget.ng_TopEdge + newGadget.ng_Height + vSpace;
 
   // Setting window title
-  SetTitle("Comparing the files");
+  SetTitle("ADiffView progress..");
 
   // Setting the window flags
   setFlags(WFLG_DRAGBAR |         // Add a drag gadget
-           WFLG_DEPTHGADGET |     // Add a depth gadget
-           WFLG_GIMMEZEROZERO);   // Different layers for border and content
-           
+           WFLG_DEPTHGADGET);     // Add a depth gadget
+
 
   // Setting the IDCMP messages we want to receive for this window
   setIDCMP(IDCMP_MENUPICK |       // Get msg when menu selected
@@ -303,6 +289,17 @@ void ProgressWindow::initialize()
            IDCMP_GADGETUP);       // Get msg when gadgets changed
 
   SetSmartRefresh(true);
+
+  // Initialize the intui text structure for progress value display
+  m_ProgressValueIText.FrontPen  = m_AScreen.Pens().HighlightedText();
+  m_ProgressValueIText.BackPen   = m_AScreen.Pens().Background();
+  m_ProgressValueIText.DrawMode  = JAM1;
+  m_ProgressValueIText.LeftEdge  = (m_ProgressBarWidth / 2) + m_ProgressBarLeft;
+
+  m_ProgressValueIText.TopEdge   = m_ProgressBarTop + (vSpace / 2) + 1;
+  m_ProgressValueIText.ITextFont = NULL;
+  m_ProgressValueIText.NextText  = NULL;
+
 
   m_bInitialized = true;
 }
