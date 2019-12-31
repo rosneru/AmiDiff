3a3ba95116cba9b519da84bdbf390d176d4e4017
Uwe Rosner <oscottmusic@posteo.de>
2019-12-31 18:47:59 +0100

    Changed app date in About command to 2020

--- src/commands/CmdAbout.cpp
+++ src/commands/CmdAbout.cpp
@@ -10,7 +10,7 @@ CmdAbout::CmdAbout(AScreen& screen,
   m_AboutMsg = pVersTag + 7;  // Skip the first 7 chars of pVersTag
                               // which is only "\0$VER: "
   m_AboutMsg += "\n\n";
-  m_AboutMsg += "Copyright(c) 2019 Uwe Rosner (u.rosner@ymail.com)";
+  m_AboutMsg += "Copyright(c) 2020 Uwe Rosner (u.rosner@ymail.com)";
   m_AboutMsg += "\n\n";
   m_AboutMsg += "This release of ADiffView may be freely distributed.\n";
   m_AboutMsg += "It may not be comercially distributed without the\n";
