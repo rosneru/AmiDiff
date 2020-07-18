c37716dbfdca9e74a47753d28f5405614c8274f6
rosneru <oscottmusic@posteo.de>
2020-07-18 09:35:59 +0200

    Create quicktests directory for *quick*tests*

---
+++ quicktests/make_test_diskpart.sh
@@ -0,0 +1,3 @@
+m68k-amigaos-g++ test_diskpart_exceptions.cpp -o test_diskpart_exceptions -noixemul
+m68k-amigaos-g++ test_diskpart_no_exceptions.cpp -o test_diskpart_no_exceptions -noixemul -fno-exceptions
+
