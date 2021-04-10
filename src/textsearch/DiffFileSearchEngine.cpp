06e99ab0797144996b1639eb4b418d54906fae13
Uwe Rosner <oscottmusic@posteo.de>
2021-04-10 16:26:09 +0200

    Added a basic text search algorithm to find the first occurence

--- src/textsearch/DiffFileSearchEngine.cpp
+++ src/textsearch/DiffFileSearchEngine.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "DiffFileSearchEngine.h"
 
 DiffFileSearchEngine::DiffFileSearchEngine(const DiffFileBase& leftFile, const DiffFileBase& rightFile)
@@ -19,10 +20,48 @@ DiffFileSearchEngine::~DiffFileSearchEngine()
 
 DiffFileSearchResult* DiffFileSearchEngine::findFirst(const char* pStrToSearch)
 {
+  for(size_t lineId = 0; lineId < m_LeftFile.getNumLines(); lineId++)
+  {
+    // Try to find pStrToSearch in left file
+    char* pFoundAt = strstr(m_LeftFile[lineId]->getText(), pStrToSearch);
+    if(pFoundAt != NULL)
+    {
+      size_t charId = pFoundAt - m_LeftFile[lineId]->getText();
 
+      if(m_pSearchResult != NULL)
+      {
+        delete m_pSearchResult;
+      }
+
+      m_pSearchResult = new DiffFileSearchResult(DiffFileSearchResult::LeftFile,
+                                                 lineId,
+                                                 charId);
+      return m_pSearchResult;
+    }
+
+    pFoundAt = strstr(m_RightFile[lineId]->getText(), pStrToSearch);
+    if(pFoundAt != NULL)
+    {
+      size_t charId = pFoundAt - m_RightFile[lineId]->getText();
+
+
+      if(m_pSearchResult != NULL)
+      {
+        delete m_pSearchResult;
+      }
+
+      m_pSearchResult = new DiffFileSearchResult(DiffFileSearchResult::RightFile,
+                                                 lineId,
+                                                 charId);
+      return m_pSearchResult;
+    }
+  }
+
+  // Not found
+  return NULL;
 }
 
 DiffFileSearchResult* DiffFileSearchEngine::findNext(const char* pStrToSearch)
 {
-
+  return NULL;
 }
