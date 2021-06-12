b052f3f1b4dbabe2a96a9df25e4181e087500ed3
Uwe Rosner <oscottmusic@posteo.de>
2021-06-12 19:55:14 +0200

    Added shortcut 'RAmiga - 3' for find next

--- src/menus/MenuDiffWindow.cpp
+++ src/menus/MenuDiffWindow.cpp
@@ -26,7 +26,7 @@ MenuDiffWindow::MenuDiffWindow(CommandBase* pCmdOpenFilesWindow,
     {   NM_ITEM,    "Next difference",      "N", 0, 0, pCmdNavNextDiff },
     {   NM_ITEM,    NM_BARLABEL,             0 , 0, 0, 0 },
     {   NM_ITEM,    "Find...",              "F", 0, 0, pCmdOpenSearchWindow },
-    {   NM_ITEM,    "Find next",             0 , 0, 0, pCmdFindNext },
+    {   NM_ITEM,    "Find next",            "3", 0, 0, pCmdFindNext },
     { NM_END,     NULL,                      0 , 0, 0, 0 },
   };
 
